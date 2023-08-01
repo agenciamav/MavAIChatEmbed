02b3189a5bee7c0c3dcb8cd4500964a7d857bd4c
Luciano T <tonetlds@gmail.com>
2023-08-01 19:46:56 -0300

    Dist branded as https://docs.flowiseai.com/how-to-use#embed

--- dist/web.d.ts
+++ dist/web.d.ts
@@ -1,11 +1,4 @@
 declare const chatbot: {
-    initFull: (props: {
-        chatflowid: string;
-        apiHost?: string | undefined;
-        chatflowConfig?: Record<string, unknown> | undefined;
-    } & {
-        id?: string | undefined;
-    }) => void;
     init: (props: {
         chatflowid: string;
         apiHost?: string | undefined;
--- dist/window.d.ts
+++ dist/window.d.ts
@@ -7,16 +7,12 @@ export declare const initFull: (props: BotProps & {
     id?: string;
 }) => void;
 export declare const init: (props: BotProps) => void;
-type Chatbot = {
-    initFull: typeof initFull;
+type MavChat = {
     init: typeof init;
 };
-export declare const parseChatbot: () => {
-    initFull: (props: BotProps & {
-        id?: string;
-    }) => void;
+export declare const parseMavChat: () => {
     init: (props: BotProps) => void;
 };
-export declare const injectChatbotInWindow: (bot: Chatbot) => void;
+export declare const injectMavChatInWindow: (bot: MavChat) => void;
 export {};
 //# sourceMappingURL=window.d.ts.map
